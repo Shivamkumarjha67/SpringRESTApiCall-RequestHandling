16467181b777a001a202dda29e490e65590c5e3e
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-25 02:54:41 +0530

    [Shivam Kumar Jha] Add . Functionality of individual greeting in controller and service layer added

--- GreetingApp/src/main/java/com/greeting/GreetingApp/Controller/GreetingController.java
+++ GreetingApp/src/main/java/com/greeting/GreetingApp/Controller/GreetingController.java
@@ -3,6 +3,7 @@ package com.greeting.GreetingApp.Controller;
 import com.greeting.GreetingApp.Service.GreetingAppService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,11 +15,20 @@ public class GreetingController {
 		this.greetingAppService = greetingAppService;
 	}
 
+	// UC3 task
+	//      route -->     localhost:9090/greeting/individual?firstName=Shivam&lastName=Kumar
+	@GetMapping("/individual")
+	public String getIndividualGreeting(@RequestParam(required = false) String firstName, @RequestParam(required = false) String lastName) {
+		return greetingAppService.getGreetingWithName(firstName, lastName);
+	}
+
+	// UC2 task
 	@GetMapping("/message")
 	public String getServiceMessage() {
 		return greetingAppService.getMessage();
 	}
 
+	// UC1 task
 	@GetMapping
 	public String getGreeting() {
 		return "Welcome to the greeting app!";
--- GreetingApp/src/main/java/com/greeting/GreetingApp/Service/GreetingAppService.java
+++ GreetingApp/src/main/java/com/greeting/GreetingApp/Service/GreetingAppService.java
@@ -4,6 +4,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingAppService {
+    // UC3 task
+    public String getGreetingWithName(String firstName, String lastName) {
+        if (firstName != null && lastName != null) {
+            return "Hello, " + firstName + " " + lastName + "!";
+        } else if (firstName != null) {
+            return "Hello, " + firstName + "!";
+        } else if (lastName != null) {
+            return "Hello, " + lastName + "!";
+        }
+
+        return getMessage();
+    }
+
+    // UC2 task
     public String getMessage() {
         return "Greeting from Service layer...";
     }
