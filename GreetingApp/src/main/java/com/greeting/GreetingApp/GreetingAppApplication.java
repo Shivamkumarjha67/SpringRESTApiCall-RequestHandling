5c6e20e889c999e6dbec3aaa5adf30694228309c
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-25 02:16:09 +0530

    [Shivam Kumar Jha] Initial Commit.

--- GreetingApp/src/main/java/com/greeting/GreetingApp/GreetingAppApplication.java
+++ GreetingApp/src/main/java/com/greeting/GreetingApp/GreetingAppApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GreetingAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingAppApplication.class, args);
+		System.out.println("Running...");
 	}
-
 }
